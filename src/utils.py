3292f9cc6563d75083bf4d1eea5a6fc659501d06
Apostolos Georgiadis <apostolos.georgiadis@nfiniity.com>
2018-01-07 12:04:04 +0100

    image preprocess added

--- src/utils.py
+++ src/utils.py
@@ -19,6 +19,26 @@ def get_classes():
             'Small-flowered Cranesbill',
             'Sugar beet']
 
+def get_green_mask(image):
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    sensitivity = 35
+    lower_hsv = np.array([60-sensitivity, 100, 50])
+    upper_hsv = np.array([60+sensitivity, 255, 255])
+    mask = cv2.inRange(image_hsv, lower_hsv, upper_hsv)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11,11))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    return mask
+
+def segment_plant(image):
+    mask = get_green_mask(image)
+    output = cv2.bitwise_and(image, image, mask = mask)
+    return output
+
+def sharpen_image(image):
+    image_blurred = cv2.GaussianBlur(image, (0, 0), 3)
+    image_sharp = cv2.addWeighted(image, 1.5, image_blurred, -0.5, 0)
+    return image_sharp
+
 def normalize(x):
     x = np.array(x, np.float32) / 255.
     #x = x.transpose((0, 1, 2, 3))
@@ -27,7 +47,9 @@ def normalize(x):
 
 def get_image(path, img_width=48, img_height=48):
     img = cv2.imread(path)
-    return cv2.resize(img, (img_width, img_height), fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    img_seg = segment_plant(img)
+    img_sharp = sharpen_image(img_seg)
+    return cv2.resize(img_sharp, (img_width, img_height), fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
 
 def load_train(path, categories, img_width, img_height):
     X_train, y_train, filenames = [], [], []
@@ -54,10 +76,11 @@ def load_test(path, img_width, img_height):
         filenames.append(filename)
     return X, filenames
 
-def get_train(path, categories, img_width, img_height):
+def get_train(path, categories, img_width, img_height, norm=True):
     X_train, y_train, filenames = load_train(path, categories, img_width, img_height)
-    X_train = normalize(X_train)
-    y_train = np.array(y_train, dtype=np.uint8)
+    if norm:
+        X_train = normalize(X_train)
+        y_train = np.array(y_train, dtype=np.uint8)
     return X_train, y_train, filenames
 
 def get_test(path, img_width, img_height):
